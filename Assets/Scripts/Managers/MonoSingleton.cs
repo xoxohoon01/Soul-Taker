a19914e4b4bd8a15487f6e5aa23b7835d0caf2f7
BL4CK DoG <xoxohoon01@gmail.com>
2024-11-26 13:51:37 +0900

    Feat: 캐릭터 생성 기능

---
+++ Assets/Scripts/Managers/MonoSingleton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get 
+        {
+            if (_instance == null)
+            {
+                GameObject newManager = new GameObject(typeof(T).Name);
+                _instance = newManager.AddComponent<T>();
+                DontDestroyOnLoad(newManager);
+            }
+            return _instance;
+        }
+    }
+
+    public bool isDontDestroyOnLoad;
+
+    protected void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            if (isDontDestroyOnLoad)
+                DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
