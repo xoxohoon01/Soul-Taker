ebae080ad8b57858314a8f5c9fe599af72d08978
Sungchan Ahn <146975762+sungchanahn@users.noreply.github.com>
2024-11-26 18:49:20 +0900

    Update: 플레이어 회전 기능, Test용 WASD 이동 추가
    Player 캐릭터가 아닌 Main mesh Renderer를 가진 자식 오브젝트를 회전

--- Assets/Scripts/Unit/PlayerController.cs
+++ Assets/Scripts/Unit/PlayerController.cs
@@ -7,14 +7,30 @@ public class PlayerController : MonoBehaviour
     //Test Variable
     public VirtualJoystick joystick;
     public Rigidbody rb;
+    public Transform mainMesh;
+    private Vector3 moveDirection;
     public float moveSpeed = 5f;
-    
+    private float horizontal;
+    private float vertical;
+
+    private void Update()
+    {
+        //Test용 입력 이동
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        moveDirection = (transform.forward * vertical + transform.right * horizontal).normalized;
+    }
+
     public void FixedUpdate()
     {
-        Vector3 direction = (Vector3.forward * joystick.Direction.y + Vector3.right * joystick.Direction.x).normalized;
-        rb.velocity = direction * moveSpeed;
+        if (joystick.Direction != Vector2.zero)
+        {
+            moveDirection = (Vector3.forward * joystick.Direction.y + Vector3.right * joystick.Direction.x).normalized;
+        }
 
-        RotationToMoveDirection(direction);
+        rb.velocity = moveDirection * moveSpeed;
+
+        RotationToMoveDirection(moveDirection);
     }
 
     private void RotationToMoveDirection(Vector3 direction)
@@ -22,6 +38,6 @@ public class PlayerController : MonoBehaviour
         if (direction == Vector3.zero) return;
 
         float rotationY = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, rotationY, 0);
+        mainMesh.rotation = Quaternion.Euler(0, rotationY, 0);
     }
 }
