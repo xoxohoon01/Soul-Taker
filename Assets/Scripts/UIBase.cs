a587aa296b07743f69a64d399300046801ffde8d
Joyoounjin <dbswls060@gmail.com>
2024-11-27 16:39:33 +0900

    Feat: UI Manager 기능 구현

---
+++ Assets/Scripts/UIBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class UIBase : MonoBehaviour
+{
+    public Canvas canvas;
+
+    //public virtual void Opened(params object[] param)
+    //{
+
+    //}
+
+    public void Hide()
+    {
+        UIManager.Instance.Hide(gameObject.name);
+    }
+}
+
