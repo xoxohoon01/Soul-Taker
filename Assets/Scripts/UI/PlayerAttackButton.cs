77cd1ae26e1e14efc719d65f346f4eab52a97761
Sungchan Ahn <146975762+sungchanahn@users.noreply.github.com>
2024-11-28 10:31:46 +0900

    Feat: Attack Button

---
+++ Assets/Scripts/UI/PlayerAttackButton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttackButton : MonoBehaviour
+{
+    [SerializeField] private GameObject coolDownUI;
+    private Image coolDownImage;
+
+    [SerializeField] private float attackCoolDownTime;
+    private float coolDownFilled;
+
+    private void Awake()
+    {
+        coolDownImage = coolDownUI.GetComponent<Image>();
+    }
+
+    public void Attack()
+    {
+        //ChangeState(stateMachine.AttackState);
+        StartCoroutine(CheckCoolDown());
+    }
+
+    IEnumerator CheckCoolDown()
+    {
+        coolDownUI.SetActive(true);
+
+        float coolDown = attackCoolDownTime;
+
+        while (coolDown >= 0)
+        {
+            coolDown -= Time.deltaTime;
+            coolDownFilled = coolDown / attackCoolDownTime;
+            coolDownImage.fillAmount = coolDownFilled;
+            yield return null;
+        }
+
+        coolDownUI.SetActive(false);
+    }
+}
