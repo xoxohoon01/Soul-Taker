5be08c933632ea8bf5f42a7d540bbb5c4cb4bf1e
Joyoounjin <dbswls060@gmail.com>
2024-12-02 10:45:28 +0900

    Add : 제이슨 데이터 파싱 스크립트 추가

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    public DialogueData dialogueData;
+    public string fileName;
+
+    [ContextMenu("To Json Data")] 
+    private void SavePlayerDataToJson()
+    {
+        string jsonData = JsonUtility.ToJson(dialogueData, true);
+        string path = Path.Combine(Application.dataPath, $"{fileName}.json");
+        File.WriteAllText(path, jsonData);
+    }
+}
+
+
