915e5df9a2ded80817993a9ef59a049cd911720e
seo600 <conrad270@naver.com>
2024-12-05 20:46:09 +0900

    Complex: ItemManager, ItemType
    Update: ItemManager
    Add: ItemType

---
+++ Assets/Scripts/UI/ItemType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using GoogleSheet.Core.Type;
+using UnityEngine;
+
+[UGS(typeof(ItemType))]
+public enum ItemType
+{
+    Equipment,
+    Consumption,
+    Misc
+}
