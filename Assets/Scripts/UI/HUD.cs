f9a38bff7a457b57afec4ad2ff652017509230d5
Joyoounjin <dbswls060@gmail.com>
2024-12-09 01:51:43 +0900

    Feat : HUD 초기화 기능 및 게임 매니저 수정
    캔버스 동적 생성으로 변경

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,11 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class HUD : UIBase
+public class HUD : Singleton<HUD>
 {
+    [SerializeField] private Canvas _canvas;
+
+    private Canvas _hudCanvasPrefab;
+    private Canvas _hudCanvas;
+
+    private GameObject _hudPrefab;
+    private GameObject _joyStickPrefab;
+
+    private GameObject _hud;
+    private GameObject _joyStick;
+
     private UIInventory inventory;
-    
+
+    public void Initialize()
+    {
+        if (_hudCanvas == null)
+        {
+            _hudCanvasPrefab = Resources.Load<Canvas>("UI/HUDCanvas");
+            _hudCanvas = Object.Instantiate(_hudCanvasPrefab);
+        }
+
+        if (_hudPrefab == null)
+        {
+            _hudPrefab = Resources.Load<GameObject>("UI/HUD");
+            _hud = Object.Instantiate(_hudPrefab, _hudCanvas.transform);
+        }
+
+        if (_joyStickPrefab == null)
+        {
+            _joyStickPrefab = Resources.Load<GameObject>("UI/Joystick");
+            _joyStick = Object.Instantiate(_joyStickPrefab, _hudCanvas.transform);
+        }
+    }
+
     public void OnInventory()
     {
         inventory = UIManager.Instance.Show<UIInventory>();
