fa6ca321a9974f596bd5fea9548ac006e2806844
Joyoounjin <dbswls060@gmail.com>
2024-12-11 16:56:24 +0900

    Feat : 오브젝트 데이터 매니저

---
+++ Assets/Scripts/Managers/ObjectDataManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using DataTable;
+
+public class ObjectDataManager : ObjectData
+{
+    public List<ObjectData> GetObject()
+    {
+        return ObjectDataList;
+    }
+
+    public ObjectData GetObjectid(int DungeonId)
+    {
+        return ObjectDataList.Find(x => x.ID == DungeonId);
+    }
+}
